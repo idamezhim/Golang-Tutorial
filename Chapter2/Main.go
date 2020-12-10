a5a4dd54e3e9c4302e642e72b7f8312427ba8339
idamezhim <70078201+idamezhim@users.noreply.github.com>
2020-12-10 12:48:32 +0100

    Update Main.go

--- Chapter2/Main.go
+++ Chapter2/Main.go
@@ -10,6 +10,24 @@ func main() {
  	fmt.Println(true || true)
  	fmt.Println(true || false)
 	fmt.Println(!true)
-	var x string = "Hello, World"
+	var x string
+ 	x = "first"
  	fmt.Println(x)
+ 	x = "second"
+	fmt.Println(x)
+	x = "hello"
+	var y string = "world"
+	fmt.Println(x == y)
+	y = "world"
+	fmt.Println(x == y)
+	y = "hello"
+	fmt.Println(x == y)
+	t := "football"
+	r := "No football"
+	fmt.Println(t == r)
+	x = "football"
+	fmt.Println(x == t)
+	g := 55
+	fmt.Println(g)
+	
 	} 
