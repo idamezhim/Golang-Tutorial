12a98a7bc6a90969223a97fbf05b37b56b1b22b6
idamezhim <70078201+idamezhim@users.noreply.github.com>
2020-12-12 00:57:33 +0100

    Create Main.go

---
+++ Exercise2/Main.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+
+func main() {
+	//This is a Program to convert Fahreheit to Celsuis
+	fmt.Print("Enter the Celsuis Degree: ")
+	var c float64
+	fmt.Scanf("%f", &c)
+	output :=  (-5 * 9/5) + 32
+	fmt.Println(output)
+}
