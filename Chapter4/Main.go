c33ff76844c8a4d922ab7c7e8b4af1915563c438
idamezhim <70078201+idamezhim@users.noreply.github.com>
2020-12-14 15:58:35 +0100

    Update Main.go

--- Chapter4/Main.go
+++ Chapter4/Main.go
@@ -3,11 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	for i := 1; i <= 10; i++ {
-	if i % 2 == 0 {
-	fmt.Println(i, "even")
-	} else {
-	fmt.Println(i, "odd")
-	}
-	}
+	fmt.Println(1)
+	fmt.Println(2)
+	fmt.Println(3)
+	fmt.Println(4)
+	fmt.Println(5)
+	fmt.Println(6)
+	fmt.Println(7)
+	fmt.Println(8)
+	fmt.Println(9)
+	fmt.Println(10)
    }
