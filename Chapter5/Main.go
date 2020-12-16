4cdbd795250c2dbd8b099557f49b30ccd433366f
idamezhim <70078201+idamezhim@users.noreply.github.com>
2020-12-17 00:27:49 +0100

    Update Main.go

--- Chapter5/Main.go
+++ Chapter5/Main.go
@@ -3,7 +3,10 @@ package main
 import "fmt"
 
 func main(){
-	var x [5]int
+	var x [19]int
 	x[4] = 100
-	fmt.Println(x)
+	x[12] = 23
+	x[3] = 24
+	x[2] = 34
+	fmt.Println(x[4], x[12], x[3], x[2], x)
 }
