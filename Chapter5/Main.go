0cf6de4a64a06a778961a3d6a02853085564e596
idamezhim <70078201+idamezhim@users.noreply.github.com>
2020-12-17 00:35:17 +0100

    Update Main.go

--- Chapter5/Main.go
+++ Chapter5/Main.go
@@ -2,11 +2,16 @@ package main
 
 import "fmt"
 
-func main(){
-	var x [19]int
-	x[4] = 100
-	x[12] = 23
-	x[3] = 24
-	x[2] = 34
-	fmt.Println(x[4], x[12], x[3], x[2], x)
-}
+func main() {
+	var x [5]float64
+	x[0] = 98
+	x[1] = 93
+	x[2] = 77
+	x[3] = 82
+	x[4] = 83
+	var total float64 = 0
+	for i := 0; i < 5; i++ {
+	total += x[i]
+	}
+	fmt.Println(total / 5)
+   }
