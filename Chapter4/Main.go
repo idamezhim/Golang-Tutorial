b5cfd556043d9bce4e2b3b8631dd6dc37d8bb374
idamezhim <70078201+idamezhim@users.noreply.github.com>
2020-12-18 23:42:54 +0100

    Update Main.go

--- Chapter4/Main.go
+++ Chapter4/Main.go
@@ -3,13 +3,15 @@ package main
 import "fmt"
 
 func main() {
-  for i := 1; i <= 10; i++{
+  for i := 1; i <= 100; i++{
 	if i % 2 == 0 {
 		fmt.Println(i , "divisible by 2") 
 		}else if i % 3 == 0 {
 		fmt.Println(i , "divisible by 3") 
 	   } else if i % 4 == 0 {
-		fmt.Println(i, "divisible by 4") 
+		fmt.Println(i, "divisible by 4")
+	   }else if i % 3 & 4 & 5 == 0 {
+		   fmt.Println(i , "Divisible by 3,4,5")
 	   }
 	}
 	
