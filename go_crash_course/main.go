fff96c6b8f5c4530f577ab5ef712c8b2be8392a2
idamezhim <70078201+idamezhim@users.noreply.github.com>
2020-12-20 10:56:51 +0100

    Update main.go

--- go_crash_course/main.go
+++ go_crash_course/main.go
@@ -1,7 +1,5 @@
 package main
-
 import "fmt"
-
 func main() {
 // declare variables
 var str string
@@ -12,20 +10,20 @@ str = "Hello World"
 n = 10
 m = 50
 mn = 2.45
-fmt.Println(str)
-fmt.Println(n)
-fmt.Println(m)
-fmt.Println(mn)
+fmt.Println("value of str= ",str)
+fmt.Println("value of n= ",n)
+fmt.Println("value of m= ",m)
+fmt.Println("value of mn= ",mn)
 // declare and assign values to variables
 var city string = "London"
 var x int = 100
-fmt.Println(city)
-fmt.Println(x)
+fmt.Println("value of city= ",city)
+fmt.Println("value of x= ",x)
 // declare variable with defining its type
 country := "DE"
 val := 15
-fmt.Println(country)
-fmt.Println(val)
+fmt.Println("value of country= ",country)
+fmt.Println("value of val= ",val)
 // define multiple variables
 var (
 name string
