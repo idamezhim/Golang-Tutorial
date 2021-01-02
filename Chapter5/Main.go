1afab8ac199a940c8f2e62f1fb32e313c2232b8f
idamezhim <70078201+idamezhim@users.noreply.github.com>
2021-01-02 01:30:40 +0100

    Update Main.go

--- Chapter5/Main.go
+++ Chapter5/Main.go
@@ -3,13 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	x := [5]float64{
-		98,
-		93,
-		77,
-		82,
-		83,
-	   }
+	x := make([]float64, 5)
 	var total float64 = 0
 for _, value := range x {
  total += value
