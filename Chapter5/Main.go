c7cc04ff55eb9472499fe73b62b2a7025d29f695
idamezhim <70078201+idamezhim@users.noreply.github.com>
2021-01-02 01:36:15 +0100

    Update Main.go

--- Chapter5/Main.go
+++ Chapter5/Main.go
@@ -3,10 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	x := make([]float64, 5)
-	var total float64 = 0
-for _, value := range x {
- total += value
-}
-fmt.Println(total / float64(len(x)))
+	slice1 := []int{1,2,3}
+	slice2 := append(slice1, 4, 5)
+	fmt.Println(slice1, slice2)
    }
