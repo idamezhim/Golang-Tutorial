1de9a709c1f8547b4a07d18788a651be3de0c02a
idamezhim <70078201+idamezhim@users.noreply.github.com>
2021-01-04 12:33:00 +0100

    Update Main.go

--- Chapter5/Main.go
+++ Chapter5/Main.go
@@ -1,18 +1,19 @@
 package main
 import "fmt"
 func main() {
- elements := make(map[string]string)
- elements["H"] = "Hydrogen"
- elements["He"] = "Helium"
- elements["Li"] = "Lithium"
- elements["Be"] = "Beryllium"
- elements["B"] = "Boron"
- elements["C"] = "Carbon"
- elements["N"] = "Nitrogen"
- elements["O"] = "Oxygen"
- elements["F"] = "Fluorine"
- elements["Ne"] = "Neon"
- name, ok := elements["Li"]
+	elements := map[string]string{
+		"H": "Hydrogen",
+		"He": "Helium",
+		"Li": "Lithium",
+		"Be": "Beryllium",
+		"B": "Boron",
+		"C": "Carbon",
+		"N": "Nitrogen",
+		"O": "Oxygen",
+		"F": "Fluorine",
+		"Ne": "Neon",
+	   }	   
+ 	name, ok := elements["Li"]
  
 fmt.Println(name, ok)
 
